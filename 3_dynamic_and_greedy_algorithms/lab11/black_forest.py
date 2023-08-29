52364d7a3a027cf1e3567c56944de3cda0a1ea87
unknown <piechnik10@gmail.com>
2023-08-29 17:38:14 +0200

    added black forest recursive algorithm

--- 3_dynamic_and_greedy_algorithms/lab11/black_forest.py
+++ 3_dynamic_and_greedy_algorithms/lab11/black_forest.py
@@ -1,6 +1,6 @@
 # we have table with trees wich each has its ow value, we cant cut two neighbouring trees. We fing max value of cutted trees
 
-def czarny_las(C):
+def black_forest(C):
     n = len(C)
     dp = [[0, 0] for _ in range(n + 1)]
 
@@ -10,5 +10,26 @@ def czarny_las(C):
     return max(dp[n][0], dp[n][1])
 
 
-print(czarny_las([1, 3, 7, 5, 6, 6, 2]))
-print(czarny_las([10, 1, 1, 10]))
+print(black_forest([1, 3, 7, 5, 6, 6, 2]))
+print(black_forest([10, 1, 1, 10]))
+
+def black_forest_recursive(TREES):
+    n = len(TREES)
+    answers = [[None, None] for _ in range(n)]
+    answers[0] = [TREES[0], 0]
+    def f(i,lastCutted): # lastCutted: 1->True, 0->False
+        if answers[i][lastCutted] != None:
+            return answers[i][lastCutted]
+        
+        if lastCutted == 1:
+            answers[i][lastCutted] = f(i-1, 0)
+        else:
+            answers[i][lastCutted] = max(f(i-1, 1) + TREES[i], f(i-1, 0))
+
+        return answers[i][lastCutted]
+    
+    return f(n-1, 0)
+
+
+print(black_forest_recursive([1, 3, 7, 5, 6, 6, 2]))
+print(black_forest_recursive([10, 1, 1, 10]))
