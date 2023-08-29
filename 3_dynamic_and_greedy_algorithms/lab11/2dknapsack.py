aa71c5814d9ad9807e1d126a924bd6408b65b325
unknown <piechnik10@gmail.com>
2023-08-29 18:07:05 +0200

    fixed poor formatting

--- 3_dynamic_and_greedy_algorithms/lab11/2dknapsack.py
+++ 3_dynamic_and_greedy_algorithms/lab11/2dknapsack.py
@@ -7,10 +7,8 @@ def knapsack(T, w, h):
     for k in range(n):
         for i in range(h, -1, -1):
             for j in range(w, -1, -1):
-                if dp[i][j] > -float(
-                        'inf') and i + T[k][1] < h + 1 and j + T[k][0] < w + 1:
-                    dp[i + T[k][1]][j + T[k][0]] = max(
-                        dp[i + T[k][1]][j + T[k][0]], dp[i][j] + T[k][2])
+                if dp[i][j] > -float('inf') and i + T[k][1] < h + 1 and j + T[k][0] < w + 1:
+                    dp[i + T[k][1]][j + T[k][0]] = max(dp[i + T[k][1]][j + T[k][0]], dp[i][j] + T[k][2])
 
     cost = -float('inf')
     for i in range(h + 1):
