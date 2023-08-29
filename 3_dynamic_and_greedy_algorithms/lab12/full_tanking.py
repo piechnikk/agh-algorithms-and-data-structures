823a99330dd4bbc65db332ba036a84fae9737089
unknown <piechnik10@gmail.com>
2023-08-29 20:31:31 +0200

    added full tanking algorithm

--- 3_dynamic_and_greedy_algorithms/lab12/full_tanking.py
+++ 3_dynamic_and_greedy_algorithms/lab12/full_tanking.py
@@ -1,14 +1,16 @@
-def full_tanking(S, b, l):
-    S.append((b, 0))
-    cost = [float('inf') for _ in range(len(S))]
-    cost[0] = S[0][1] * l
+# an array is given with the distance of the station from 0 and the price of fuel at the station S = [(distance, price), ...]
+# tank capacity = T
+# you have to get to station n-1 at the lowest cost
+# you have to always fill up to a full tank
 
-    for i in range(len(S)):
-        j = i+1
-
-        while j < len(S) and S[j][0] <= S[i][0] + l:
-            cost[j] = min(cost[j], cost[i] + (S[j][0] - S[i][0]) * S[j][1])
-            j+=1
+def full_tanking(S, T): # S[i][0 -> distance, 1 -> price]
+    n = len(S)
+    cost = S[0][1] * T
+    position = 0 # actual position
+    while position+T < S[n-1][0]:
+        best = min(list(filter((lambda x: (x[0]>position and x[0]<=position+T)), S)), key=lambda x: x[1])
+        cost += (best[0]-position) * best[1]
+        position = best[0]
     return cost
-S = [(0, 1), (6, 3), (7, 5), (14, 6)]
-print(full_tanking(S, 20, 7))
+S = [(0, 1), (6, 3), (7, 5), (14, 6), (20, 0)]
+print(full_tanking(S,7))
