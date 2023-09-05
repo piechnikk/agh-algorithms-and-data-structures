72249d6a353b271628b17c5d2959780d7462e28c
pawel <piechnik10@gmail.com>
2023-09-05 10:29:50 +0200

    added comments to algorithm b from exam

--- exam/B-planety/egz1b_correct.py
+++ exam/B-planety/egz1b_correct.py
@@ -6,22 +6,27 @@ inf = float("inf")
 def planets( D, C, T, E ):
     n = len(D)
     answers = [[inf] * (E + 1) for _ in range(n)]
+    
+    # answers for start planet
     for b in range(E + 1):
         answers[0][b] = b * C[0]
-    if T[0][0] != 0:
+    if T[0][0] != 0: # teleport
         answers[T[0][0]][0] = min(answers[T[0][0]][0], answers[0][0] + T[0][1])
     
     for i in range(1, n):
         for b in range(E + 1):
             distance = D[i] - D[i - 1]
-            answers[i][b] = min(
-                [answers[i][b], answers[i - 1][b + distance] if b + distance <= E else inf] +
-                list(answers[i - 1][b + distance - j] + j * C[i - 1] if b + distance <= E else inf for j in range(b + distance + 1)))
-        if T[i][0] != i:
+            possible_costs = [answers[i][b]] # teleport
+            if b + distance <= E:
+                possible_costs.append(answers[i - 1][b + distance]) # without refueling
+                for j in range(b + distance + 1):
+                    possible_costs.append(answers[i - 1][b + distance - j] + j * C[i - 1]) # with refueling j fuel
+
+            answers[i][b] = min(possible_costs)
+        if T[i][0] != i: # teleport from i to T[i][0] planet
             answers[T[i][0]][0] = min(answers[T[i][0]][0], answers[i][0] + T[i][1])
+        
     return min(answers[n-1])
         
-
-
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( planets, all_tests = True )
